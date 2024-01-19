1bc47c618233e639e908b600f1ded4df638e5fa2
Joe Ikedo <joeikedo@gmail.com>
2024-01-18 19:30:59 -0800

    added apng2gif exe call

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import os
 
 DOWNLOAD_FOLDER_PATH = 'C:/Users/joeik/OneDrive/Documents/Code Repos/LINE-Stamp-Downloader/Downloaded_Images'
 LINE_STAMP_STORE_URL = 'https://stickershop.line-scdn.net/sticonshop/v1/sticon/640fe3e67e1f2c21dbd02b15/iPhone/001_animation.png?v=1'
+APNG2GIF_EXE_PATH = '"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"' # Executeable needs to be put in double quotes to be run by os.system()
+GIF_FOLDER_PATH = 'C:/Users/joeik/OneDrive/Documents/Code Repos/LINE-Stamp-Downloader/GIF_Images'
 LINE_STAMP_FILE_EXTENSION = '.png' # All LINE Stamps I know of are in APNG (.png) format
+STAMP_SET_NAME = 'Jersey-chan-emoji-6'
 
+def convertPNGstoGIFs():
+    imageFileNameList = os.listdir(DOWNLOAD_FOLDER_PATH)
+    for imageName in imageFileNameList:
+        windowsCommandString = APNG2GIF_EXE_PATH + ' ' + imageName + ' ' + GIF_FOLDER_PATH + '/' + imageName[:-4] + '.gif'
+        #os.system(windowsCommandString)
+    
 
 # Helper function for downloadEmojiSet() that sets the url from .../iPhone/001_animation.png... to .../iPhone/002_animation.png... etc, based on input index number.
 def adjustEmojiSetURL(inputIndex):
@@ -28,14 +37,15 @@ def downloadEmojiSet():
     for i in range(NUMBER_OF_EMOJI_STAMPS):
         currentIterationImageURL = adjustEmojiSetURL(i + 1) # Start at 1 not 0
         img_data = requests.get(currentIterationImageURL).content
-        completeFilePath = os.path.join(DOWNLOAD_FOLDER_PATH, 'Line_Emoji' + str(i + 1)  + LINE_STAMP_FILE_EXTENSION) # use os.path.join to add path of specific folder to save image to.  
+        completeFilePath = os.path.join(DOWNLOAD_FOLDER_PATH, STAMP_SET_NAME + str(i + 1)  + LINE_STAMP_FILE_EXTENSION) # use os.path.join to add path of specific folder to save image to.  
         with open(completeFilePath, 'wb') as handler:
             handler.write(img_data)
 
 
 
 def main():
-    downloadEmojiSet()
+    #downloadEmojiSet()
+    convertPNGstoGIFs()
 
 
 if __name__ == "__main__":
